a1da5f105dab6ccce7ab16df46a17ade621d81b0
bryant <BryantSISU@gmail.com>
2017-05-12 03:12:39 +0800

    添加详情页和单页缓存装饰器

--- blog_main_part/views.py
+++ blog_main_part/views.py
@@ -1,18 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
 from .models import Article
 
 
+@cache_page(60 * 15)
 def home(request):
     """主页，及其数据处理逻辑"""
     post_list = Article.objects.all()  # 获取Article模型的全部数据
     return render(request, 'blog_main_part/home.html', {'post_list': post_list})
 
 
+@cache_page(60 * 15)
 def detail(request, my_args):
     """文章详情页，及其数据处理逻辑"""
-    post = Article.objects.all()[int(my_args)]
-    str = ("title=%s, category=%s, date_added=%s, content=%s"
-           % (post.title, post.category, post.date_added, post.content))
-    return HttpResponse(str)
+    post = Article.objects.get(id=int(my_args))
+    return render(request, 'blog_main_part/detail.html', {'post': post})
