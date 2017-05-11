d8bc9bff902a67e288953079861e2b48330bbbc1
bryant <BryantSISU@gmail.com>
2017-05-12 05:46:24 +0800

    设置里的语言改为中文和时区为中国

--- my_first_blog/settings.py
+++ my_first_blog/settings.py
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'  # 把英文改为中文
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'  # 改为中国时区
 
 USE_I18N = True
 
