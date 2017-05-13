fdcff07a61515349d6a81ef20811779bf6cb2c8a
bryant <BryantSISU@gmail.com>
2017-05-14 06:39:39 +0800

    提交模板标签文件

---
+++ blog_main_part/templatetags/blog_main_part_tags.py
@@ -0,0 +1,24 @@
+from django import template
+
+from .models import Category, Post
+
+# 实例化一个模板库，给python函数提供装饰器，使之能使用模板标签语法
+register = template.Library()
+
+
+@register.simple_tag  # 这样这个函数就能在模板中用便签语法了（ {%  %} )
+def get_recent_posts(num=5):
+    """一个获取数据中前num篇文章的函数，提供给模板标签，做文章侧边栏列表"""
+    return Post.objects.all()[:num]
+
+
+@register.simple_tag  # 如法炮制
+def archives():
+    """通过时间归档列表"""
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+
+@register.simple_tag  # 如法炮制
+def get_categories():
+    """给归档的列表分类"""
+    return Category.objects.all()
